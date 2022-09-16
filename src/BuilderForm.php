75a42459a23cd6eebd68b11f4b9775c7d0dcab22
JorgRios <www.fer_gha@hotmail.com>
2022-09-16 17:25:23 -0400

    Se agregaron el selector de pestañas header

--- src/BuilderForm.php
+++ src/BuilderForm.php
@@ -2,24 +2,60 @@
 
 namespace Jrios\ApexBuilder;
 
+use Illuminate\Support\HtmlString;
+use Illuminate\Support\Facades\Route;
 class BuilderForm
 {
 
     /**
      * Get hello word from library.
      *
-     * @return string
+     * @return cadena
      */
-    protected static function getHello()
-    {
-        return 'Hola mundo';
-    }
-
+    
     public function getTest($name = null)
     {
         if (is_null($name)) {
             $name = "Anonimo";
         }
-        return view('templates.hola', compact('name'));
+        return 'Hola <b>'.$name.'</b>';
     }
+
+    /**
+     * Genera las pestañas del navegador
+     */
+
+    public function header($elements = [])
+    {
+        if(count($elements)>0){
+            $text = '<ul class="nav nav-tabs">';
+            foreach ($elements as $key => $element) {
+                $text .= '<li class="nav-item">';
+                $text .= '<a  class="nav-link ';
+                if(Route::is($element['route'])){
+                    $text .= 'active"';
+                }else{
+                    $text .= 'text-muted" ';
+                }
+                $text .= ' aria-current="page" ';
+                $text .= 'href="';
+                $text .= route($element['route']);
+                $text .= '">'.$element['name'].'</a><li>';
+            }
+            $text .= '</ul>';
+        }else{
+            $text = "";
+        }
+        return $text;
+    }
+
+    /**
+     * 
+     */
+    public function table()
+    {
+
+    }
+
+
 }
